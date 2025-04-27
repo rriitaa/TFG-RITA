9eec210e1a91dc13c1082de8f4ce695494a27402
Rita <rita.barragan@usp.ceu.es>
2025-04-27 23:15:32 +0200

    borrado todas las carpetas y estructura

--- gym/gym/backend/logout.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-session_start();
-session_unset(); // Eliminar todas las variables de sesión
-session_destroy(); // Destruir la sesión
-
-header("Location: login.html"); // Redirigir al login
-exit();
-?>
